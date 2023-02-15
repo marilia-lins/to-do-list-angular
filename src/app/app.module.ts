6770181284edee8c48a4e127a9ecbe1086d1e2e4
marilia-lins <121808358+marilia-lins@users.noreply.github.com>
2023-02-15 15:30:36 -0300

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { TodoInputAddItensComponent } from './modules/home/components/todo-input-add-itens/todo-input-add-itens.component';
+import { TodoListComponent } from './modules/home/components/todo-list/todo-list.component';
+import { HomeModule } from './modules/home/home.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HomeModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
